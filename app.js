43afe71e913ec298252a3088abbb8c0001aedaac
Joey Schrader <joedanproperty@gmail.com>
2018-03-02 05:40:25 -0700

    testing new user routes, server still working

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const path = require('path');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+
+//LIBRARIES
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy
 const mongoose = require('mongoose');
 
 const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost/more_chore'
@@ -16,8 +20,9 @@ mongoose.connection
 
 const app = express();
 
+//CONTROLLERS
+const auth = require('./routes/auth');
 const chores = require('./routes/chores');
-const users = require('./routes/users');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -25,8 +30,28 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.resolve(__dirname, './client/build')));
 
+//EXPRESS SESSION SET UP
+app.use(require('express-session')({
+  secret: process.env.SESSION_SECRET || 'secret',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    httpOnly: false,
+    secure: false
+  }
+}));
+
+//SETUP PASSPORT
+app.use(passport.initialize());
+app.use(passport.session());
+const User = require('./models/user');
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//ROUTES
+app.use('/api/auth', auth);
 app.use('/api/chores', chores);
-app.use('/api/users', users)
 
 app.get('*', (request, response) => {
    response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
